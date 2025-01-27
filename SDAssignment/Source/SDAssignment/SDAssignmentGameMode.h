c4832dcf69c933b1713e20ae5e4d1bc741b00f57
FilipW94 <filipwilhelmsson94@gmail.com>
2025-01-27 10:34:55 +0100

    Initial commit + some level prototyping

---
+++ SDAssignment/Source/SDAssignment/SDAssignmentGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SDAssignmentGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASDAssignmentGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASDAssignmentGameMode();
+};
+
+
+
