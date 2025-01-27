c2b3c1d38e966637f461b0f6a3897a5ca0d4d9d9
FilipW94 <filipwilhelmsson94@gmail.com>
2025-01-27 13:33:34 +0100

    Added currencysubsystem

---
+++ SDAssignment/Source/SDAssignment/Private/CurrencySubsystem.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "CurrencySubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UCurrencySubsystem : public UGameInstanceSubsystem
+{
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+private:
+	GENERATED_BODY()
+
+	UPROPERTY()
+	uint32 TotalScore;
+
+	UPROPERTY()
+	uint32 SessionScore;
+	
+};
